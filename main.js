a47bec162234b142a68d51fe45c3409d66274e25
orbutbulschool <orbutbul@coastal.edu>
2023-05-25 23:55:42 -0400

    Made Shader Material work with string literals

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 
+
 let number = "zzzzzzzzzzzz";
 let b36Num= parseInt(number,36);
 console.log(b36Num);
@@ -15,22 +16,24 @@ document.body.appendChild( renderer.domElement );
 
 //Shader Material 
 
-// const boxMaterial = new THREE.ShaderMaterial({
-// 	wireframe: true,
-// 	vertexShader: `
-// 	void main()	{
-// 	  // projectionMatrix, modelViewMatrix, position -> passed in from Three.js
-// 	  gl_Position = projectionMatrix
-// 		* modelViewMatrix
-// 		* vec4(position.x, position.y, position.z, 1.0);
-// 	}
-// 	`,
-// 	fragmentShader: `
-// 	void main() {
-// 	  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-// 	}
-// 	`,
-//   });
+const boxMaterial = new THREE.ShaderMaterial({
+	// wireframe: true,	
+	vertexShader: `
+      void main()	{
+        vec4 result;
+        result = vec4(position.x, position.y, position.z, 1.0);
+        gl_Position = projectionMatrix
+          * modelViewMatrix
+          * result;
+      }
+      `,
+
+	fragmentShader: `
+       void main() {
+         gl_FragColor = vec4(.5, 0.5, 0.0, 1.0);
+       }
+       `,
+});
 
 //scene
 
@@ -40,7 +43,9 @@ scene.background = new THREE.Color("rgb(214,195,144)");
 
 //Orbit Controls
 
-
+// const boxMaterial = new THREE.MeshPhongMaterial({
+// 	color: 0xff0000,
+// } );
 
 
 //Camera
@@ -49,7 +54,9 @@ camera.position.set( 0, 10, 50 );
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
 
-
+// const box = new THREE.BoxGeometry(1,1,1,1,1,1);
+// const boxmesh = new THREE.Mesh(box,boxMaterial);
+// scene.add(boxmesh);
 
 const loader = new GLTFLoader();
 
@@ -66,11 +73,11 @@ function gltfModel(path){
 		});
 
 
-		scene.add(loadedobject)
+		scene.add(loadedobject);
 	})
 
 }
-gltfModel('Cube.glb');
+gltfModel('Sq antiprism.glb');
 
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
